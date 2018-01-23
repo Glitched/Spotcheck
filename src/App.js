eeb24f97d6422ab6b87d8fe7d0e0d7a3303fa7db
Ryan Slama <rockinryguy@gmail.com>
2018-01-23 16:25:06 -0500

    Added Textbox styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Heading from './components/Heading'
+import Textbox from './components/Textbox'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Heading>Spotcheck</Heading>
+        <Textbox placeholder="Code"/>
+        <Textbox placeholder="Username"/>
       </div>
     );
   }
---
+++ src/components/Textbox.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled, { css } from 'react-emotion'
+
+const StyledInput = styled('input')`
+  background: rgba(0,0,0,0.5);
+  border: 1px solid rgba(255,255,255,0.4);
+  border-radius: 4px;
+  padding: 8px;
+  color: white;
+  font-family: Lato;
+  font-weight: 400;
+  font-size: 18px;
+  text-transform: uppercase;
+  transition: all 0.2s ease;
+  margin: 8px auto;
+  display: block;
+  &:hover,
+  &:focus {
+    border: 1px solid #3CCDCC;
+    box-shadow: 0 2px 11px 0 #41D1BE;
+    outline-width: 0;
+  }
+`
+
+class Textbox extends React.Component {
+  render() {
+    return (
+      <StyledInput type="text" placeholder={this.props.placeholder}/>
+    );
+  }
+}
+
+export default Textbox;
